c609a0261dc53e1a3066dd45661f5ce43d6c6153
cristian685 <cristi.popina98@gmail.com>
2021-06-08 01:05:11 +0300

    Add the entities of the books fragment

--- app/src/main/java/com/example/tema3android/activities/fragments/BooksFragment.java
+++ app/src/main/java/com/example/tema3android/activities/fragments/BooksFragment.java
@@ -4,20 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tema3android.R;
 
 
 public class BooksFragment extends Fragment {
 
+    private EditText mTitleEditText;
+    private EditText mAuthorEditText;
+    private EditText mDescriptionEditText;
+    private RecyclerView mBooksRecyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_books,container,false);
+
+        mTitleEditText = view.findViewById(R.id.bookTitleEditText);
+        mAuthorEditText = view.findViewById(R.id.bookAuthorEditText);
+        mDescriptionEditText = view.findViewById(R.id.bookDescriptionEditText);
+
+        mBooksRecyclerView = view.findViewById(R.id.bookRecyclerView);
 
         return view;
     }
